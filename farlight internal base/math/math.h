d557701b09c508e5a763fd3be677b0a110d96bf4
Emir <77904942+ReFo0@users.noreply.github.com>
2023-10-31 01:35:05 +0300

    Update math.h

--- farlight internal base/math/math.h
+++ farlight internal base/math/math.h
@@ -5,10 +5,9 @@
 #include <cstring>
 #include <iostream>
 #include <cmath>
-#include <d3d9.h>
-#include "../../../../../../Program Files (x86)/Microsoft DirectX SDK (June 2010)/Include/d3dx9math.h"
-#define powFFFFFFFFFFFFFFFFFFFFFF(n) (n)*(n)
-#define M_PI 3.14159265358979323846264338327950288419716939937510
+#include <d3d9types.h>
+
+#define ___(n) (n)*(n)
 
 struct vec2
 {
@@ -38,8 +37,8 @@ struct vec3
 	float distance(vec3 vec)
 	{
 		return sqrt(
-			powFFFFFFFFFFFFFFFFFFFFFF(vec.x - x, 2) +
-			powFFFFFFFFFFFFFFFFFFFFFF(vec.y - y, 2)
+			___(vec.x - x, 2) +
+			___(vec.y - y, 2)
 		);
 	}
 
@@ -228,10 +227,6 @@ struct vec3
 	}
 };
 
-float distancee(vec3 vec)
-{
-	return sqrt(pow(vec.x - vec.x, 2) + pow(vec.y - vec.y, 2));
-}
 
 struct FQuat
 {
@@ -292,149 +287,12 @@ struct FTransform
 	}
 };
 
-struct FVector
-{
-	float                                              X;                                                         // 0x0000(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-	float                                              Y;                                                         // 0x0004(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-	float                                              Z;                                                         // 0x0008(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-
-
-	inline FVector() : X(0), Y(0), Z(0) {}
-
-	inline FVector(float x, float y, float z) : X(x), Y(y), Z(z) {}
-
-	inline FVector operator + (const FVector& other) const { return FVector(X + other.X, Y + other.Y, Z + other.Z); }
-
-	inline FVector operator - (const FVector& other) const { return FVector(X - other.X, Y - other.Y, Z - other.Z); }
-
-	inline FVector operator * (float scalar) const { return FVector(X * scalar, Y * scalar, Z * scalar); }
-
-	inline FVector operator * (const FVector& other) const { return FVector(X * other.X, Y * other.Y, Z * other.Z); }
-
-	inline FVector operator / (float scalar) const { return FVector(X / scalar, Y / scalar, Z / scalar); }
-
-	inline FVector operator / (const FVector& other) const { return FVector(X / other.X, Y / other.Y, Z / other.Z); }
-
-	inline FVector& operator=  (const FVector& other) { X = other.X; Y = other.Y; Z = other.Z; return *this; }
-
-	inline FVector& operator+= (const FVector& other) { X += other.X; Y += other.Y; Z += other.Z; return *this; }
-
-	inline FVector& operator-= (const FVector& other) { X -= other.X; Y -= other.Y; Z -= other.Z; return *this; }
-
-	inline FVector& operator*= (const float other) { X *= other; Y *= other; Z *= other; return *this; }
-
-	inline float Dot(const FVector& b) const { return (X * b.X) + (Y * b.Y) + (Z * b.Z); }
-
-	inline float MagnitudeSqr() const { return Dot(*this); }
-
-	inline float Magnitude() const { return std::sqrtf(MagnitudeSqr()); }
-
-	inline FVector Unit() const
-	{
-		const float fMagnitude = Magnitude();
-		return FVector(X / fMagnitude, Y / fMagnitude, Z / fMagnitude);
-	}
-
-
-	friend bool operator==(const FVector& first, const FVector& second) { return first.X == second.X && first.Y == second.Y && first.Z == second.Z; }
-
-	friend bool operator!=(const FVector& first, const FVector& second) { return !(first == second); }
-
-};
-struct FRotator
-{
-	float                                              Pitch;                                                     // 0x0000(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-	float                                              Yaw;                                                       // 0x0004(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-	float                                              Roll;                                                      // 0x0008(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-
-
-	inline FRotator() : Pitch(0), Yaw(0), Roll(0) {}
-
-	inline FRotator(float pitch, float yaw, float roll) : Pitch(pitch), Yaw(yaw), Roll(roll) {}
-
-	inline float Size() { return sqrt(Pitch * Pitch + Yaw * Yaw + Roll * Roll); }
-
-	inline FRotator Clamp()
-	{
-		FRotator r = { Pitch, Yaw, Roll };
-		if (r.Yaw > 180.f)
-			r.Yaw -= 360.f;
-		else if (r.Yaw < -180.f)
-			r.Yaw += 360.f;
-
-		if (r.Pitch > 180.f)
-			r.Pitch -= 360.f;
-		else if (r.Pitch < -180.f)
-			r.Pitch += 360.f;
-
-		if (r.Pitch < -89.f)
-			r.Pitch = -89.f;
-		else if (r.Pitch > 89.f)
-			r.Pitch = 89.f;
-
-		r.Roll = 0.f;
-		return r;
-	}
-
-	inline FRotator operator + (const FRotator& other) const { return FRotator(Pitch + other.Pitch, Yaw + other.Yaw, Roll + other.Roll); }
-
-	inline FRotator operator - (const FRotator& other) const { return FRotator(Pitch - other.Pitch, Yaw - other.Yaw, Roll - other.Roll); }
-
-	inline FRotator operator * (float scalar) const { return FRotator(Pitch * scalar, Yaw * scalar, Roll * scalar); }
-
-	inline FRotator operator * (const FRotator& other) const { return FRotator(Pitch * other.Pitch, Yaw * other.Yaw, Roll * other.Roll); }
-
-	inline FRotator operator / (float scalar) const { return FRotator(Pitch / scalar, Yaw / scalar, Roll / scalar); }
-
-	inline FRotator operator / (const FRotator& other) const { return FRotator(Pitch / other.Pitch, Yaw / other.Yaw, Roll / other.Roll); }
-
-	inline FRotator& operator=  (const FRotator& other) { Pitch = other.Pitch; Yaw = other.Yaw; Roll = other.Roll; return *this; }
-
-	inline FRotator& operator+= (const FRotator& other) { Pitch += other.Pitch; Yaw += other.Yaw; Roll += other.Roll; return *this; }
-
-	inline FRotator& operator-= (const FRotator& other) { Pitch -= other.Pitch; Yaw -= other.Yaw; Roll -= other.Roll; return *this; }
-
-	inline FRotator& operator*= (const float other) { Pitch *= other; Yaw *= other; Roll *= other; return *this; }
-
-
-};
-struct FVector2D
-{
-	float                                              X;                                                         // 0x0000(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-	float                                              Y;                                                         // 0x0004(0x0004) (Edit, BlueprintVisible, ZeroConstructor, SaveGame, IsPlainOldData, NoDestructor, HasGetValueTypeHash, NativeAccessSpecifierPublic)
-
-
-	inline FVector2D() : X(0), Y(0) {}
-
-	inline FVector2D(float x, float y) : X(x), Y(y) {}
-
-	inline FVector2D operator + (const FVector2D& other) const { return FVector2D(X + other.X, Y + other.Y); }
-
-	inline FVector2D operator - (const FVector2D& other) const { return FVector2D(X - other.X, Y - other.Y); }
-
-	inline FVector2D operator * (float scalar) const { return FVector2D(X * scalar, Y * scalar); }
-
-	inline FVector2D operator * (const FVector2D& other) const { return FVector2D(X * other.X, Y * other.Y); }
-
-	inline FVector2D operator / (float scalar) const { return FVector2D(X / scalar, Y / scalar); }
-
-	inline FVector2D operator / (const FVector2D& other) const { return FVector2D(X / other.X, Y / other.Y); }
-
-	inline FVector2D& operator=  (const FVector2D& other) { X = other.X; Y = other.Y; return *this; }
-
-	inline FVector2D& operator+= (const FVector2D& other) { X += other.X; Y += other.Y; return *this; }
-
-	inline FVector2D& operator-= (const FVector2D& other) { X -= other.X; Y -= other.Y; return *this; }
-
-	inline FVector2D& operator*= (const float other) { X *= other; Y *= other; return *this; }
-
-};
 
 class FMinimalViewInfo
 {
 public:
-	FVector Location; // 0x00(0x0c)
-	FRotator Rotation; // 0x0c(0x0c)
+	vec3 Location; // 0x00(0x0c)
+	vec3 Rotation; // 0x0c(0x0c)
 	float FOV; // 0x18(0x04)
 	float DesiredFOV; // 0x1c(0x04)
 	float OrthoWidth; // 0x20(0x04)
